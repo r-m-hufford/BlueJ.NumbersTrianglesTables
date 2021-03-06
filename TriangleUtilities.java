b54e27fd0eab0791a3a1be66f52ce4f6c6699056
r-m-hufford <ryan@rmhufford.com>
2021-03-06 14:06:36 -0500

    complete

--- TriangleUtilities.java
+++ TriangleUtilities.java
@@ -3,19 +3,47 @@
 public class TriangleUtilities {
 
     public static String getRow(int numberOfStars) {
-        return null;
+        String stars = "";
+        for (int i = 0; i < numberOfStars; i++) {
+            stars += "*";
+        }
+        return stars;
     }
     
     public static String getTriangle(int numberOfRows) {
-        return null;
+        String triangle = "";
+
+        for (int i = 0; i < numberOfRows; i++) {
+            for (int j = 0; j <= i; j++) {
+                triangle += "*";
+            }
+            triangle += "\n";
+        }
+        return triangle;
     }
 
 
     public static String getSmallTriangle() {
-        return null;
+        String triangle = "";
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j <= i; j++) {
+                triangle += "*";
+            }
+            triangle += "\n";
+        }
+        return triangle;
     }
 
-    public static String getLargeTriangle() {
-        return null;
+    public static String getLargeTriangle()  {
+        String triangle = "";
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j <= i; j++) {
+                triangle += "*";
+            }
+            triangle += "\n";
+        }
+        return triangle;
     }
 }
